07587ec78e9f8f7db8caa5712ca814ae998ab016
dshush <dani.shosh@gmail.com>
2012-12-19 20:52:10 +0200

    add the HashFunction interface

---
+++ src/cryptographic/primitives/HashFunction.java
@@ -0,0 +1,21 @@
+package cryptographic.primitives;
+
+/**
+ * An interface for Hash Function, which is an algorithm or subroutine that maps 
+ * large data sets of variable length, called keys, to smaller data sets of a fixed 
+ * length.
+ */
+public interface HashFunction {
+	
+	/**
+	 * @param input the byte array to run the hash function on.
+	 * @return the hash digest of the byte array input using the hash function. 
+	 */
+	public byte[] digest(byte[] input);
+	
+	/**
+	 * @return the number of bits in the output of the hash function.
+	 */
+	public int outlen();
+	
+}
