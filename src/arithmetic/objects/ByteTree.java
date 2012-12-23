2e0ec4d641be36493f3cb51d7d727b3b8ed8d636
Itay <itayva@hotmail.com>
2012-12-23 18:38:29 +0200

    added byte tree interface

---
+++ src/arithmetic/objects/ByteTree.java
@@ -0,0 +1,14 @@
+package arithmetic.objects;
+
+/**
+ * This is a "wrapper" interface used to denote each arithmetic object as a byte tree.
+ *
+ */
+public interface ByteTree {
+
+	/**
+	 * @return the byte array representation of the byte tree.
+	 */
+	public byte[] toByteArray();
+
+}
