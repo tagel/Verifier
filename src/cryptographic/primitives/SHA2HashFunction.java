60715f4a6dd95b4dbafd7abb3dd9775491321155
dshush <dani.shosh@gmail.com>
2012-12-27 20:31:46 +0200

    implementation and tests for SHA2HashFunction

--- src/cryptographic/primitives/SHA2HashFunction.java
+++ src/cryptographic/primitives/SHA2HashFunction.java
@@ -2,37 +2,46 @@ package cryptographic.primitives;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * This class provides the functionality of a message digest algorithm from the "SHA-2" 
- * family, using the java.security.MessageDigest class.
+ * This class provides the functionality of a message digest algorithm from the
+ * "SHA-2" family, using the java.security.MessageDigest class.
  */
 public class SHA2HashFunction implements HashFunction {
-	
+
 	private final MessageDigest md;
-	
+	private final List<String> SHA2_FAMILY_ALGORITHMS = Arrays.asList(
+			"SHA-256", "SHA-384", "SHA-512");
+
 	/**
-	 * @param algorithm the name of the hash algorithm to use, must be in the "SHA-2" family.
-	 * @throws IllegalArgumentException if the algorithm is not one of the "SHA-2" family.
+	 * @param algorithm
+	 *            the name of the hash algorithm to use, must be in the "SHA-2"
+	 *            family.
+	 * @throws IllegalArgumentException
+	 *             if the algorithm is not one of the "SHA-2" family.
 	 */
 	public SHA2HashFunction(String algorithm) throws IllegalArgumentException {
 		try {
-			this.md = MessageDigest.getInstance(algorithm);
-		} catch (NoSuchAlgorithmException e) {			
+			if (SHA2_FAMILY_ALGORITHMS.contains(algorithm)) {
+				this.md = MessageDigest.getInstance(algorithm);
+			} else {
+				throw new IllegalArgumentException();
+			}
+		} catch (NoSuchAlgorithmException e) {
 			throw new IllegalArgumentException();
 		}
 	}
-	
+
+	// TODO Daniel - add tests!
 	@Override
 	public byte[] digest(byte[] input) {
-		// TODO Daniel: complete 
-		return null;
+		return md.digest(input);
 	}
 
 	@Override
 	public int outlen() {
-		// TODO Daniel: complete 
-		return 0;
+		return (md.getDigestLength() * 8);
 	}
-
 }
