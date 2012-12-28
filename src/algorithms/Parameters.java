99c50c3c52b7b1cb2a858dbce9f5cd5e3262a196
dshush <dani.shosh@gmail.com>
2012-12-28 17:25:50 +0200

    Merged Tagel changes

--- src/algorithms/Parameters.java
+++ src/algorithms/Parameters.java
@@ -1,11 +1,11 @@
 package algorithms;
 
-import arithmetic.objects.*;
+import java.math.BigInteger;
 
 import arithmetic.objects.ArrayOfElements;
 import arithmetic.objects.GroupElement;
 import arithmetic.objects.IGroup;
-
+import arithmetic.objects.ProductElement;
 
 /**
  * This class describes an object that contains the parameters used by the
@@ -15,17 +15,30 @@ import arithmetic.objects.IGroup;
  */
 public class Parameters {
 
-	MixParams[] mix;
-	
-	// The following parameters are created during the run
+	public Parameters(String protInfo, String directory, String type, java.lang.String auxsid,
+			BigInteger w, boolean posc, boolean ccpos, boolean dec ) {
+		super();
+		this.auxsid = auxsid;
+		this.protInfo = protInfo;
+		this.directory = directory;
+		this.type = type;
+		this.w = w;
+		this.posc = posc;
+		this.ccpos = ccpos;
+		this.dec = dec;
+		
+	}
+
 	private byte[] prefixToRO;
 	private IGroup Gq;
 
 	// parameters from directory
+	private String protInfo;
+	private String directory;
 	private String version;
 	private String type;
 	private String auxsid;
-	private int w;
+	private BigInteger width;
 	private ProductElement fullPublicKey;
 	private int maxciph;
 
@@ -34,80 +47,246 @@ public class Parameters {
 	private String sessionID;
 	private int numOfParties;
 	private int threshold;
-	private int Ne;
-	private int Nr;
-	private int Nv;
+	private BigInteger Ne;
+	private BigInteger Nr;
+	private BigInteger Nv;
 	private String sh;
 	private String sGq;
 	private String sPRG;
-	private int wDeafult;
+	private BigInteger w;
+	private BigInteger wDefault;
 
 	// parameters from CMD
+	private String typeExpected;
 	private boolean posc;
 	private boolean ccpos;
 	private boolean dec;
 	private String auxidExp;
-	private int widthExp;
-	private String directory;
-	private String protInfo;
-	private String type_expected;
+	private BigInteger widthExp;
 
-	// From lists
+	/**
+	 * @return the Verificatum version
+	 */
+	public String getVersion() {
+		return version;
+	}
+
+	/**
+	 * @return the proof type: "mixing" \ "shuffling" \ "decryption"
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @return the auxiliary session identifier of this session
+	 */
+	public String getAuxsid() {
+		return auxsid;
+	}
+
+	/**
+	 * @return the width w>0 of a ciphertext
+	 */
+	public BigInteger getWidth() {
+		return width;
+	}
+
+	/**
+	 * @return the full public key
+	 */
+	public ProductElement getFullPublicKey() {
+		return fullPublicKey;
+	}
+
+	/**
+	 * @return the number N0 of ciphertexts for which pre-computation was
+	 *         performed
+	 */
+	public int getMaxciph() {
+		return maxciph;
+	}
+
+	// ***FROM CMD***
+
+	/**
+	 * @return true, if it is a proof of shuffle commitment and false otherwise
+	 */
+	public boolean getPosc() {
+		return posc;
+	}
+
+	/**
+	 * Sets true - if this is proof of shuffle commitment and false otherwise
+	 */
+	public void setPosc(boolean posc) {
+		this.posc = posc;
+	}
+
+	/**
+	 * @return true if it is commitment-consistent proof of a shuffle and false
+	 *         otherwise
+	 */
+	public boolean getCcpos() {
+		return ccpos;
+	}
+
+	/**
+	 * Sets true - if this is commitment-consistent proof of a shuffle and false
+	 * otherwise
+	 */
+	public void setCcpos(boolean ccpos) {
+		this.ccpos = ccpos;
+	}
+
+	/**
+	 * @return true if it is proof of correct decryption and false otherwise
+	 */
+	public boolean getDec() {
+		return dec;
+	}
+
+	/**
+	 * Sets true - if this is proof of correct decryption and false otherwise
+	 */
+	public void setDec(boolean dec) {
+		this.dec = dec;
+	}
+
+	public void setTypeExpected(String typeExpected) {
+		this.typeExpected = typeExpected;
+	}
+
+	public String getTypeExpected() {
+		return typeExpected;
+	}
+
+	// *****FROM XML*******
+
+	/**
+	 * @return the version id of the verificatum.
+	 */
+	public String getProtVersion() {
+		return protVersion;
+	}
+
+	/**
+	 * @return the globally unique session identifier tied to the generation of
+	 *         a particular joint public key
+	 */
+	public String getSessionID() {
+		return sessionID;
+	}
+
+	/**
+	 * @return the number of parties (k).
+	 */
+	public int getNumOfParties() {
+		return numOfParties;
+	}
+
+	/**
+	 * @return the threshold = number of mix-servers that take part in the
+	 *         shuffling
+	 */
+	public int getThreshold() {
+		return threshold;
+	}
+
+	/**
+	 * @return the number of bits in each component of random vectors used for
+	 *         batching in proofs of shuffles and proofs of correct decryption.
+	 */
+	public BigInteger getNe() {
+		return Ne;
+	}
+
+	/**
+	 * @return the acceptable statistical error when sampling random values.
+	 */
+	public BigInteger getNr() {
+		return Nr;
+	}
+
+	/**
+	 * @return the number of bits used in the challenge of the verifier in
+	 *         zero-knowledge proofs.
+	 */
+	public BigInteger getNv() {
+		return Nv;
+	}
+
+	/**
+	 * @return the hash function used to create the random oracles.
+	 */
+	public String getSh() {
+		return sh;
+	}
+
+	/**
+	 * @return the string representation of the group Gp.
+	 */
+	public String getsGq() {
+		return sGq;
+	}
+
+	/**
+	 * @return the hash function used to create the PRG used to expand
+	 *         challenges into arrays.
+	 */
+	public String getsPRG() {
+		return sPRG;
+	}
+
+	/**
+	 * @return the default width of cipher-texts and plain-texts.
+	 */
+	public BigInteger getW() {
+		return w;
+	}
+
+	public BigInteger getwDefault() {
+		return wDefault;
+	}
+
+	public void setwDefault(BigInteger wDefault) {
+		this.wDefault = wDefault;
+	}
+
+	// parameters from lists
 	private ArrayOfElements<GroupElement> ciphertexts;
 	private ArrayOfElements<GroupElement> ShuffledCiphertexts;
 	private ArrayOfElements<GroupElement> plaintexts;
 
 	/**
-	 * Fills the params from the cmd
+	 * @return the input ciphertexts
 	 */
-	public Parameters(String protInfo, String directory, String type,
-			String auxid, int w, boolean posc, boolean ccpos, boolean dec) {
-		this.posc = posc;
-		this.ccpos = ccpos;
-		this.dec = dec;
-		this.auxidExp = auxid;
-		this.widthExp = w;
-		this.directory = directory;
-		this.protInfo = protInfo;
-		this.type_expected = type;
-
-		prefixToRO = null;
-		Gq = null;
-		version = null;
-		type = null;
-		auxsid = null;
-		w = 0;
-		fullPublicKey = null;
-		protVersion = null;
-		sessionID = null;
-		numOfParties = 0;
-		threshold = 0;
-		Ne = 0;
-		Nr = 0;
-		Nv = 0;
-		sh = null;
-		sGq = null;
-		sPRG = null;
-		wDeafult = 0;
-		maxciph = 0;
-		
-		mix = null;
-		
-		}
-
-	// fill the relevant parameters from the given xml
-	// fill the relevant parameters:
-	// versionprot, sid, k, thresh, ne, nr, nv, sH, sPRG, sGq , and wdefault
-	// (width);
-	public boolean fillFromXML() {
-		mix = new MixParams[threshold];
-		return true;
+	public ArrayOfElements<GroupElement> getCiphertexts() {
+		return ciphertexts;
 	}
 
-	// fill the relevant parameters from the given directory
-	// fill version_proof(Version) type, auxid, w from proof directory
-	public boolean fillFromDirectory() {
-		return true;
+	/**
+	 * @return the re-randomized and permuted ciphertexts (for the shuffling
+	 *         session)
+	 */
+	public ArrayOfElements<GroupElement> getShuffledCiphertexts() {
+		return ShuffledCiphertexts;
+	}
+
+	/**
+	 * @return the output plaintext elements that has not been decoded in any
+	 *         way
+	 */
+	public ArrayOfElements<GroupElement> getPlaintexts() {
+		return plaintexts;
+	}
+
+	/**
+	 * Fills the parameters (from xml, directory, lists but NOT from the cmd)
+	 */
+	public boolean fillParams() {
+		return (fillFromDirectory() && fillFromXML());
+
 	}
 
 	/**
@@ -116,15 +295,31 @@ public class Parameters {
 	public boolean readLists() {
 		return true;
 	}
-	
-		
-	//******Getters and Setters***********
-	public byte[] getPrefixToRO() {
-		return prefixToRO;
+
+	// fill the relevant parameters from the given xml
+	public boolean fillFromXML() {
+		return true;
 	}
 
-	public void setPrefixToRO(byte[] prefixToRO) {
-		this.prefixToRO = prefixToRO;
+	// fill the relevant parameters from the given directory
+	public boolean fillFromDirectory() {
+		return true;
+	}
+
+	public String getAuxidExp() {
+		return auxidExp;
+	}
+
+	public void setAuxidExp(String auxidExp) {
+		this.auxidExp = auxidExp;
+	}
+
+	public BigInteger getWidthExp() {
+		return widthExp;
+	}
+
+	public void setWidthExpected(BigInteger widthExpected) {
+		this.widthExp = widthExpected;
 	}
 
 	public IGroup getGq() {
@@ -134,136 +329,5 @@ public class Parameters {
 	public void setGq(IGroup gq) {
 		Gq = gq;
 	}
-
-	public ProductElement getFullPublicKey() {
-		return fullPublicKey;
-	}
-
-	public void setFullPublicKey(ProductElement fullPublicKey) {
-		this.fullPublicKey = fullPublicKey;
-	}
-
-	public ArrayOfElements<GroupElement> getCiphertexts() {
-		return ciphertexts;
-	}
-
-	public void setCiphertexts(ArrayOfElements<GroupElement> ciphertexts) {
-		this.ciphertexts = ciphertexts;
-	}
-
-	public ArrayOfElements<GroupElement> getShuffledCiphertexts() {
-		return ShuffledCiphertexts;
-	}
-
-	public void setShuffledCiphertexts(
-			ArrayOfElements<GroupElement> shuffledCiphertexts) {
-		ShuffledCiphertexts = shuffledCiphertexts;
-	}
-
-	public ArrayOfElements<GroupElement> getPlaintexts() {
-		return plaintexts;
-	}
-
-	public void setPlaintexts(ArrayOfElements<GroupElement> plaintexts) {
-		this.plaintexts = plaintexts;
-	}
-
-	public String getVersion() {
-		return version;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public String getAuxsid() {
-		return auxsid;
-	}
-
-	public int getW() {
-		return w;
-	}
-
-	public String getProtVersion() {
-		return protVersion;
-	}
-
-	public String getSessionID() {
-		return sessionID;
-	}
-
-	public int getNumOfParties() {
-		return numOfParties;
-	}
-
-	public int getThreshold() {
-		return threshold;
-	}
-
-	public int getNe() {
-		return Ne;
-	}
-
-	public int getNr() {
-		return Nr;
-	}
-
-	public int getNv() {
-		return Nv;
-	}
-
-	public String getSh() {
-		return sh;
-	}
-
-	public String getsGq() {
-		return sGq;
-	}
-
-	public String getsPRG() {
-		return sPRG;
-	}
-
-	public int getwDeafult() {
-		return wDeafult;
-	}
-
-	public boolean isPosc() {
-		return posc;
-	}
-
-	public boolean isCcpos() {
-		return ccpos;
-	}
-
-	public boolean isDec() {
-		return dec;
-	}
-
-	public String getAuxidExp() {
-		return auxidExp;
-	}
-
-	public int getWidthExp() {
-		return widthExp;
-	}
-
-	public String getDirectory() {
-		return directory;
-	}
-
-	public String getProtInfo() {
-		return protInfo;
-	}
-
-	public String getType_expected() {
-		return type_expected;
-	}
-
-	public void setMaxciph(int maxciph) {
-		this.maxciph = maxciph;
-	}
-		
-
 
 }
--- src/algorithms/VerKeys.java
+++ src/algorithms/VerKeys.java
@@ -1,48 +1,73 @@
 package algorithms;
 
+import java.util.List;
+
 import arithmetic.objects.ArrayOfElements;
-import arithmetic.objects.FieldElement;
+import arithmetic.objects.ByteTree;
+import arithmetic.objects.GroupElement;
 import arithmetic.objects.IGroup;
+import arithmetic.objects.Node;
+import arithmetic.objects.ProductElement;
 
 public class VerKeys {
+
+
 //TODO : tagel remove me!
 	private MixParams mixParams;
+	private Parameters params;
 	
-	public ArrayOfElements verifyKeys(int lambda, IGroup Gq) {
-		MixParams [] mixServers = new MixParams [lambda];
+	public Node verifyKeys(int lambda, IGroup Gq) {
 		
-		// Joint public key
-		ProductGroupElement pk = params.getFullPublicKey();		
+		
+		// read the joint public key
+		ProductElement pk = params.getFullPublicKey();
 		if (pk == null) {
 			// TODO : tagel remove me!
 			return null;
 		}
+		//ArrayOfElements<GroupElement> pkList = new pk.toList();
+		//GroupElement g = pkList.get(0);
+		//GroupElement y = pkList.get(1);
 		
-		// Public keys
-		//TODO: attempt to read y1...ylambda
-		int [] publicKeys = new int [lambda]; //y1,y2,...,yl
-
-		int mult = 1;
-		for (int l = 0; l < lambda; l++) {
-			// TODO: need a helpFunc group to BigInt
-			publicKeys[l] = mixParams.getPublicKey();
-			mult *= publicKeys[l];
-		}
-		// TODO: understand who us y, where does it come from
-		if (y != mult) {
-			return null;
-		}
+		// read the public keys of the mix servers
 		
-		// Secret keys
-		int [] secretKeys = new int [lambda]; //x1,x2,...,xl
-		for (int l = 0; l < lambda; l++) {
-			FieldElement secretKeyFile = mixServers[lambda].getSecretKey();
-			if (secretKeyFile != null && ){
-				return null;
-				
-			}
-		}
 	
+		
+//		MixParams [] mixServers = new MixParams [lambda];		
+//		// Joint public key
+//		ProductGroupElement pk = params.getFullPublicKey();		
+//		if (pk == null) {
+//			return null;
+//		}
+//		
+//		// Public keys
+//		//TODO: attempt to read y1...ylambda
+//		int [] publicKeys = new int [lambda]; //y1,y2,...,yl
+//
+//		int mult = 1;
+//		for (int l = 0; l < lambda; l++) {
+//			// TODO: need a helpFunc group to BigInt
+//			publicKeys[l] = mixParams.getPublicKey();
+//			mult *= publicKeys[l];
+//		}
+//		// TODO: understand who us y, where does it come from
+//		if (y != mult) {
+//			return null;
+//		}
+//		
+//		// Secret keys
+//		int [] secretKeys = new int [lambda]; //x1,x2,...,xl
+//		for (int l = 0; l < lambda; l++) {
+//			FieldElement secretKeyFile = mixServers[lambda].getSecretKey();
+//			if (secretKeyFile != null && ){
+//				return null;
+//				
+//			}
+//		}
+//	
+		ByteTree [] a = new ByteTree [3];  
+		Node retVal = new Node(a);
+		return retVal;
 	}
 	
 	
