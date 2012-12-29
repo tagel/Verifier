3fcc679a57f5a0c4015c1b3d4cb10852d1493db7
tagel <tagelshaked@gmail.com>
2012-12-29 16:22:36 +0200

    adding constructor

--- src/algorithms/ProveShuffling.java
+++ src/algorithms/ProveShuffling.java
@@ -1,27 +1,51 @@
 package algorithms;
 
-import algorithms.params.Parameters;
+import java.math.BigInteger;
+
+import arithmetic.objects.ArrayOfElements;
+import arithmetic.objects.GroupElement;
+import arithmetic.objects.IField;
+import arithmetic.objects.IGroup;
+import arithmetic.objects.Node;
+import arithmetic.objects.ProductElement;
+import cryptographic.primitives.PseudoRandomGenerator;
 import cryptographic.primitives.RandomOracle;
 
 /**
- * This class provides the functionality of proving the correctness of re-encryption and permutation of the input ciphertexts.
+ * This class provides the functionality of proving the correctness of
+ * re-encryption and permutation of the input ciphertexts.
+ * 
  * @author Tagel & Sofi
  */
 public class ProveShuffling {
 
 	private RandomOracle ROSeed;
 	private RandomOracle ROChallenge;
-	
+
 	/**
 	 * @return true if the proof of shuffle was correct and false otherwise.
 	 */
-	public ProveShuffling(RandomOracle ROSeed, RandomOracle ROChallenge){
+	public ProveShuffling(RandomOracle ROSeed, RandomOracle ROChallenge) {
 		this.ROSeed = ROSeed;
 		this.ROChallenge = ROChallenge;
 	}
+
+	public boolean prove(
+			byte[] ro, int N, 
+			BigInteger Ne, BigInteger Nr, BigInteger Nv,
+			PseudoRandomGenerator prg,
+			IGroup Gq, int Rw, int Cw,
+			ProductElement pk,
+			BigInteger wInput, BigInteger wOutput,
+			ArrayOfElements<ArrayOfElements<GroupElement>> permutationCommitment,
+			ArrayOfElements<Node> PoSCommitment, ArrayOfElements<Node> PoSReply) {
 	
-	public boolean prove(Parameters params) {
-		// TODO Auto-generated method stub
+			BigInteger q = Gq.getFieldOrder();
+            IField Zq = new Field(q);
+            
+            //GroupElement ATag = new ModGroupElement(PoSCommitment.
+            
+			
 		return false;
 	}
 
