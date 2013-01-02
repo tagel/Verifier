3b2adbc0e723d605f384ec083a211a8959650cb7
tagel <tagelshaked@gmail.com>
2013-01-02 13:13:07 +0200

    stage 1 in the algorithm

--- src/algorithms/ProveShuffling.java
+++ src/algorithms/ProveShuffling.java
@@ -3,11 +3,16 @@ package algorithms;
 import java.math.BigInteger;
 
 import arithmetic.objects.ArrayOfElements;
+import arithmetic.objects.ByteTree;
+import arithmetic.objects.ElementsExtractor;
 import arithmetic.objects.GroupElement;
 import arithmetic.objects.IField;
 import arithmetic.objects.IGroup;
+import arithmetic.objects.IntegerFieldElement;
 import arithmetic.objects.Node;
+import arithmetic.objects.PrimeOrderField;
 import arithmetic.objects.ProductElement;
+import arithmetic.objects.ProductGroupElement;
 import cryptographic.primitives.PseudoRandomGenerator;
 import cryptographic.primitives.RandomOracle;
 
@@ -31,21 +36,68 @@ public class ProveShuffling {
 	}
 
 	public boolean prove(
-			byte[] ro, int N, 
-			BigInteger Ne, BigInteger Nr, BigInteger Nv,
+			byte[] ro,
+			int N,
+			int Ne,
+			int Nr,
+			int Nv,
 			PseudoRandomGenerator prg,
-			IGroup Gq, int Rw, int Cw,
-			ProductElement pk,
-			BigInteger wInput, BigInteger wOutput,
+			IGroup Gq,
+			IGroup Rw,
+			IGroup Cw,
+			ProductGroupElement pk,
+			ArrayOfElements<GroupElement> wInput,
+			ArrayOfElements<GroupElement> wOutput,
 			ArrayOfElements<ArrayOfElements<GroupElement>> permutationCommitment,
-			ArrayOfElements<Node> PoSCommitment, ArrayOfElements<Node> PoSReply) {
-	
-			BigInteger q = Gq.getFieldOrder();
-            IField Zq = new Field(q);
-            
-            //GroupElement ATag = new ModGroupElement(PoSCommitment.
-            
-			
+			Node PoSCommitment, Node PoSReply) {
+
+		/** 
+		 * 1(b) - interpret Tpos as Node(B,A',B',C',D',F')
+		 */
+		ByteTree [] PosCommitmentArr = PoSCommitment.getChildrenArray();
+
+		// creating B,A',B',C',D',F'
+		GroupElement [] BRaw = new GroupElement[N];
+		GroupElement [] BTagRaw = new GroupElement[N];
+		
+		// TODO: check how to interpret B,B'
+		for (int i = 0; i < N i++) {
+			BRaw[i] = ElementsExtractor.createGroupElement(PosCommitmentArr[0], Gq);
+			BTagRaw[i] = ElementsExtractor.createGroupElement(PosCommitmentArr[2], Gq);
+		}
+		
+		ArrayOfElements<GroupElement> B = new ArrayOfElements<GroupElement>(BRaw);
+		ArrayOfElements<GroupElement> Btag = new ArrayOfElements<GroupElement>(BTagRaw);
+		
+		GroupElement Atag = ElementsExtractor.createGroupElement(PosCommitmentArr[1], Gq);
+		GroupElement Ctag = ElementsExtractor.createGroupElement(PosCommitmentArr[3], Gq);
+		GroupElement Dtag = ElementsExtractor.createGroupElement(PosCommitmentArr[4], Gq);
+		GroupElement Ftag = ElementsExtractor.createGroupElement(PosCommitmentArr[5], Gq);
+		 
+		/** 
+		 * 1(c) interpret Opos as Node(Ka,Kb,Kc,Kd,Ke,Kf)
+		 */
+		ByteTree [] PosReplyArr = PoSReply.getChildrenArray();
+		BigInteger q = Gq.getFieldOrder();
+		IField<IntegerFieldElement> Zq = new PrimeOrderField(q);
+		IntegerFieldElement Ka = new IntegerFieldElement (ElementsExtractor.leafToInt(PosReplyArr[0].toByteArray()),Zq);
+		IntegerFieldElement Kc = new IntegerFieldElement (ElementsExtractor.leafToInt(PosReplyArr[2].toByteArray()),Zq);
+		IntegerFieldElement Kd = new IntegerFieldElement (ElementsExtractor.leafToInt(PosReplyArr[3].toByteArray()),Zq);
+		GroupElement Kf = ElementsExtractor.createGroupElement(PosReplyArr[5], Gq);
+		
+		IntegerFieldElement [] KbRaw = new IntegerFieldElement[N];
+		IntegerFieldElement [] KeRaw = new IntegerFieldElement[N];
+		
+		// TODO: check how to interpret Ke,Kb
+		for (int i = 0; i < N i++) {
+			KbRaw[i] = ElementsExtractor.createGroupElement(PosCommitmentArr[0], Gq);
+			KeRaw[i] = ElementsExtractor.createGroupElement(PosCommitmentArr[2], Gq);
+		}
+		
+		ArrayOfElements<IntegerFieldElement> Kb = new ArrayOfElements<IntegerFieldElement>(KbRaw);
+		ArrayOfElements<IntegerFieldElement> Ke = new ArrayOfElements<IntegerFieldElement>(KeRaw);
+		
+		
 		return false;
 	}
 
