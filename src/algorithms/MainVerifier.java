bb07a47b4a0053e475024590308eaf5d008ad3fe
sofiazax <sofiazax@gmail.com>
2013-01-08 20:51:58 +0200

    ..

--- src/algorithms/MainVerifier.java
+++ src/algorithms/MainVerifier.java
@@ -91,7 +91,7 @@ public class MainVerifier {
 
 		// *******Section 4 in the Algorithm*********
 		createPrefixToRo();
-		
+
 		// *******Section 5 in the Algorithm*********
 		if (!ReadKeys())// Attempt to read was unsuccessful
 			return false;
@@ -100,35 +100,53 @@ public class MainVerifier {
 		if (!ReadLists())
 			return false;
 
+		// *******Section 7 in the Algorithm*********
+		// Here we call the subroutines of the verifier
+		//7a
+		if ((params.getType().equals("mixing") || params.getType().equals(
+				"decryption"))
+				&& (params.isPosc() || params.isCcpos()))
+
+			if (!VerShuffling.verify(params.getPrefixToRO(),
+					params.getThreshold(), params.getNe(), params.getNr(),
+					params.getNe(), params.getPrg(), params.getGq(),
+					params.getFullPublicKey(), params.getCiphertexts(),
+					params.getShuffledCiphertexts(), params.isPosc(),
+					params.isCcpos()))
+				return false;
+		
+		//7b
+		if ((params.getType().equals("mixing") || params.getType().equals(
+				"decryption")) && params.isDec())
+	
+		//TODO:finish here!
+
 		return true;
 	}
 
 	/**
-	 * @return true if successfully derived Gq, Zq, Mw, Cw, Rw, the hash function H
-	 *         and the PRG and false otherwise.
+	 * @return true if successfully derived Gq, Zq, Mw, Cw, Rw, the hash
+	 *         function H and the PRG and false otherwise.
 	 */
 	private boolean deriveSetsAndObjects() {
-		//Unmarshall Gq
+		// Unmarshall Gq
 		try {
 			params.setGq(ElementsExtractor.unmarshal(params.getsGq()));
 		} catch (UnsupportedEncodingException e) {
 			System.out.println("Error unmarshalling Gq");
 			return false;
 		}
-		
-		//Derive the Zq prime order Field
-		BigInteger q = params.getGq().getFieldOrder();
-		IField<IntegerFieldElement> Zq = new PrimeOrderField(q);
-		params.setZq(Zq);
 
-		//Set the Hashfunction and the pseudo random generator
+		// create the field
+		params.setZq(new PrimeOrderField(params.getGq().getFieldOrder()));
+
+		// Set the Hashfunction and the pseudo random generator
 		H = new SHA2HashFunction(params.getSh());
 		params.setPrg(new HashFuncPRG(new SHA2HashFunction(params.getsPRG())));
-		
+
 		return true;
 	}
-	
-	
+
 	private void createPrefixToRo() {
 		String s = params.getSessionID() + "." + params.getAuxsid();
 		ByteTree btAuxid = new StringLeaf(s);
@@ -157,13 +175,12 @@ public class MainVerifier {
 		byte[] Seed = node.toByteArray();
 
 		params.setPrefixToRO(H.digest((Seed)));
-		}
-	
-	
+	}
+
 	private boolean ReadKeys() {
-		//TODO: Change the file names and the paths so it will be generic 
-		//TODO: and will fit to the new constructor.
-		ProductGroupElement pk = newProductGroupElement(
+		// TODO: Change the file names and the paths so it will be generic
+		// TODO: and will fit to the new constructor.
+		ProductGroupElement pk = ElementsExtractor.createSimplePGE(
 				ElementsExtractor.btFromFile(params.getDirectory(),
 						"FullPublicKey.bt"), params.getGq());
 		IGroupElement y = pk.getArr()[1];
@@ -179,10 +196,10 @@ public class MainVerifier {
 
 		for (i = 0; i < params.getThreshold(); i++) {
 			// Here we assume that the file exists
-			yi = ElementsExtractor.createGroupElement(
-					ElementsExtractor.btFromFile(params.getDirectory(),
-							"proofs","PublicKey" + (i < 10 ? "0" : "")
-							+ (i + 1))+".bt", params.getGq());
+			yi = ElementsExtractor.createGroupElement(ElementsExtractor
+					.btFromFile(params.getDirectory(), "proofs", "PublicKey"
+							+ (i < 10 ? "0" : "") + (i + 1) + ".bt"), params
+					.getGq());
 			if (yi == null)
 				return false;
 			params.getMixPublicKey().add(yi);
@@ -192,27 +209,25 @@ public class MainVerifier {
 		if (!res.equal(y))
 			return false;
 
-		//TODO: concat the file name correctly
+		// TODO: The bt from file returns NULL, not the FieldElement!!!
 		// Here we check the secret keys - xi:
 		// Here the file can be null
 		for (i = 0; i < params.getThreshold(); i++) {
 			xi = new IntegerFieldElement(
-					ElementsExtractor.leafToInt(ElementsExtractor
-							.btFromFile(params.getDirectory(),"/proofs/"
-									+ "SecretKey0" + (i < 10 ? "0" : "")
-									+ (i + 1))), params.getZq());
+					ElementsExtractor.leafToInt(ElementsExtractor.btFromFile(
+							params.getDirectory(), "proofs", "SecretKey"
+									+ (i < 10 ? "0" : "") + (i + 1) + ".bt")),
+					params.getZq());
 			params.getMixSecretKey().add(xi);
-			//xi = null if the file doesn't exist
+			// xi = null if the file doesn't exist
 		}
-		
-		
-		//Check the 3.b part of keys verifier
+
+		// Check the 3.b part of keys verifier
 		for (i = 0; i < params.getThreshold(); i++) {
 			xi = params.getMixSecretKey().getAt(i);
 			yi = params.getMixPublicKey().getAt(i);
-			
-			if ((xi != null) &&
-					!(yi.equal(g.power(xi.getElement()))))
+
+			if ((xi != null) && !(yi.equal(g.power(xi.getElement()))))
 				return false;
 		}
 
@@ -220,15 +235,58 @@ public class MainVerifier {
 
 	}
 
+	// Part 6 of the algorithm
 	private boolean ReadLists() {
-		String filename = "name";
-		
-		
+		// section 6a of the Algorithm
+		byte[] file = ElementsExtractor.btFromFile(params.getDirectory(),
+				"Ciphertexts.bt");
+		if (file == null)
+			return false;
+
+		ArrayOfElements<ProductGroupElement> ciphertexts = ElementsExtractor
+				.createArrayOfCiphertexts(file, params.getGq());
+
+		params.setN(ciphertexts.getSize());
+
+		// section 6b of the Algorithm
+		// if the type==mixing, read the file ShuffledCiphertexts.bt from
+		// Directory
+		// if the type==shuffling, read the file Ciphertexts_threshold from
+		// Directory/proofs
+		if (params.getType().equals("mixing")) {
+			file = ElementsExtractor.btFromFile(params.getDirectory(),
+					"ShuffledCiphertexts.bt");
+			if (file == null)
+				return false;
+		} else if (params.getType().equals("shuffling")) {
+			file = ElementsExtractor.btFromFile(params.getDirectory(),
+					"proofs",
+					"Ciphertexts" + (params.getThreshold() < 10 ? "0" : "")
+							+ params.getThreshold() + ".bt");
+			if (file == null)
+				return false;
+		}
+
+		ArrayOfElements<ProductGroupElement> ShuffledCiphertexts = ElementsExtractor
+				.createArrayOfCiphertexts(file, params.getGq());
+		params.setShuffledCiphertexts(ShuffledCiphertexts);
+
+		// Section 6c of the algorithm
+		if (params.getType().equals("mixing")
+				|| params.getType().equals("decryption")) {
+			file = ElementsExtractor.btFromFile(params.getDirectory(),
+					"Plaintexts.bt");
+			if (file == null)
+				return false;
+
+			ArrayOfElements<ProductGroupElement> plaintexts = ElementsExtractor
+					.createArrayOfCiphertexts(file, params.getGq());
+			params.setPlaintexts(plaintexts);
+		}
+
 		return true;
 	}
-	
-	
-	
+
 	/**
 	 * @return true if the proof params are valid and false otherwise.
 	 */
@@ -237,7 +295,4 @@ public class MainVerifier {
 		return false;
 	}
 
-	
-
-	
 }
--- src/algorithms/VerShuffling.java
+++ src/algorithms/VerShuffling.java
@@ -1,5 +1,10 @@
 package algorithms;
 
+import arithmetic.objects.ArrayOfElements;
+import arithmetic.objects.Groups.IGroup;
+import arithmetic.objects.Groups.ProductGroupElement;
+import cryptographic.primitives.PseudoRandomGenerator;
+
 /**
  * This class provides the functionality of verifying the shuffling.
  * @author Tagel & Sofi
@@ -8,9 +13,21 @@ package algorithms;
 public class VerShuffling {
 
 	/**
+	 * @param c 
+	 * @param b 
+	 * @param arrayOfElements2 
+	 * @param arrayOfElements 
+	 * @param productGroupElement 
+	 * @param iGroup 
+	 * @param pseudoRandomGenerator 
+	 * @param l 
+	 * @param k 
+	 * @param j 
+	 * @param i 
+	 * @param bs 
 	 * @return true if verification of shuffling was successful and false otherwise.
 	 */
-	public boolean verify() {
+	static public boolean verify(byte[] bs, int i, int j, int k, int l, PseudoRandomGenerator pseudoRandomGenerator, IGroup iGroup, ProductGroupElement productGroupElement, ArrayOfElements<ProductGroupElement> arrayOfElements, ArrayOfElements<ProductGroupElement> arrayOfElements2, boolean b, boolean c) {
 		// TODO Auto-generated method stub
 		return false;
 	}
