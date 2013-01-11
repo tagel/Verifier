b2be0ad49fb9fc6eff2b5df334f58f0dbb32ed4a
sofiazax <sofiazax@gmail.com>
2013-01-12 00:00:28 +0200

    Finished shanks tonelli algorithm

--- src/arithmetic/objects/groups/ECurveRandArray.java
+++ src/arithmetic/objects/groups/ECurveRandArray.java
@@ -1,7 +1,8 @@
 package arithmetic.objects.groups;
 
-import java.math.BigInteger;
 
+
+import arithmetic.objects.LargeInteger;
 import arithmetic.objects.arrays.ArrayOfElements;
 import cryptographic.primitives.PseudoRandomGenerator;
 
@@ -14,7 +15,7 @@ public class ECurveRandArray {
 	
 	ArrayOfElements<IGroupElement> Rand;
 	
-	public ECurveRandArray(BigInteger q, int N, PseudoRandomGenerator prg,
+	public ECurveRandArray(LargeInteger q, int N, PseudoRandomGenerator prg,
 			byte[] seed, int nr) {
 		
 		ArrayOfElements<IGroupElement> RandArray = new ArrayOfElements<IGroupElement>();
@@ -25,9 +26,10 @@ public class ECurveRandArray {
 		
 		for (int i = 0; i < N; i++) {
 			byte[] arr = prg.getNextPRGOutput(length);
-			BigInteger t = new BigInteger(arr);
-			BigInteger ttag = t.mod(BigInteger.valueOf(2).pow(nq+nr));
-			BigInteger zi = ttag.mod(q);
+			LargeInteger t = new LargeInteger(arr);
+			LargeInteger ttag = t.mod(LargeInteger.valueOf(2).pow(nq+nr));
+			LargeInteger zi = ttag.mod(q);
+			//TODO finish the array after the shanks tonelli works
 		}
 		
 		
@@ -36,51 +38,128 @@ public class ECurveRandArray {
 	}
 	
 	
-	
-	
-		
-		
-	
-	public ArrayOfElements<IGroupElement> getRand() {
-		return Rand;
-	}
+	public LargeInteger[] shanksTonelli(LargeInteger a, LargeInteger p) {
+		LargeInteger[] retVal = new LargeInteger[2];
 
+		// Verify that the Lagendre symbol of a and p is not -1:
+		if (Legendre(a, p) == -1)
+			return null;
 
+		// Define Q and s to be p-1=Q2^s when Q is odd
+		LargeInteger[] powers = findPowers(p);
+		LargeInteger Q = powers[1];
+		LargeInteger s = powers[0];
 
+		// Define b as a the least quadratic non residual
+		LargeInteger b = findLeastQNR(p);
 
+		// Find a's opposite in the modular field
+		LargeInteger atag = a.modInverse(p);
 
+		// compute c
+		LargeInteger c = b.modPow(Q, p);
 
+		// compute R
+		LargeInteger r = a.modPow(
+				Q.add(LargeInteger.ONE).divide(LargeInteger.valueOf(2)), p);
 
-	public int[] shanksTonelli(int a, int p) {
-		int[] retVal = new int[2];
-		
+		LargeInteger i = LargeInteger.ONE;
+		LargeInteger d;
+
+		for (i = LargeInteger.ONE; !i.equals(s); ) {
+			// compute the power as s-i-1
+			LargeInteger pow = LargeInteger.valueOf(2).pow(
+					s.intValue() - i.intValue() - 1);
+			d = (r.pow(2).multiply(atag).modPow(pow, p));
+
+			if (d.equals(p.subtract(LargeInteger.ONE)))
+				r = r.multiply(c).mod(p);
+			c = c.modPow(LargeInteger.valueOf(2), p);
+			
+			i = i.add(LargeInteger.ONE);
+
+		}
+
+		retVal[0] = r;
+		retVal[1] = LargeInteger.ZERO.subtract(r).add(p); // -r mod p
+
 		return retVal;
 	}
-	
+
 	/**
-	 * Find positive integers Q and S such that p-1=Q*2^s, where p is a prime number
-	 * it means that p-1 is even.
+	 * 
+	 * @param p
+	 *            - a prime number
+	 * @return the first quadratic non residue of p
+	 */
+	public LargeInteger findLeastQNR(LargeInteger p) {
+		LargeInteger retVal = LargeInteger.ONE;
+		boolean flag = false;
+		while (!flag) {
+			retVal = retVal.add(LargeInteger.ONE);
+			if (Legendre(retVal, p) == -1)
+				return retVal;
+			else
+				retVal = retVal.add(LargeInteger.ONE);
+			if (retVal.compareTo(p) >= 0)
+				return retVal;
+		}
+
+		// The function shouldn't get here at all. Half of the values should be
+		// quadratic non residues of p
+		return retVal;
+	}
+
+	/**
+	 * @param a
+	 *            - integer
+	 * @param p
+	 *            - prime
+	 * @return the Legendre symbol of a / b
+	 */
+	public int Legendre(LargeInteger a, LargeInteger p) {
+		if (a.remainder(p).equals(LargeInteger.ZERO)) {
+			return 0;
+		}
+		LargeInteger exponent = p.subtract(LargeInteger.ONE);
+		exponent = exponent.divide(LargeInteger.valueOf(2));
+		LargeInteger result = a.modPow(exponent, p); // 1 <= result <= p - 1
+
+		if (result.equals(LargeInteger.ONE)) {
+			return 1;
+		} else if (result.equals(p.subtract(LargeInteger.ONE))) {
+			return -1;
+		} else {
+			throw new ArithmeticException(
+					"Error computing the Legendre symbol.");
+		}
+	}
+
+	/**
+	 * Find positive integers Q and S such that p-1=Q*2^s, where p is a prime
+	 * number it means that p-1 is even.
+	 * 
 	 * @param p
 	 * @return an array of 2 integers where {s,Q}
 	 */
-	public static int[] findPowers(int p) {
-		int[] retVal = new int[2];
-		int s=0;
-		int Q=p-1;//p-1 is even
-		
-		//Q needs to be odd
-		while (Q%2==0) {
-			Q = Q/2;
-			s++;
-		} 
-		
+	public LargeInteger[] findPowers(LargeInteger p) {
+		LargeInteger[] retVal = new LargeInteger[2];
+		LargeInteger s = LargeInteger.ZERO;
+		LargeInteger Q = p.subtract(LargeInteger.ONE);// p-1 is even
+		// Q needs to be odd
+		while (Q.mod(LargeInteger.valueOf(2)).equals(LargeInteger.ZERO))  {
+			Q = Q.divide(LargeInteger.valueOf(2));
+			s = s.add(LargeInteger.ONE);
+		}
 		retVal[0] = s;
 		retVal[1] = Q;
-		
 		return retVal;
 	}
+	
 
-
+	public ArrayOfElements<IGroupElement> getRand() {
+		return Rand;
+	}
 
 
 }
