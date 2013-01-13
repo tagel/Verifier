aa0e07845c8e209a3b5e89cdedcc50f573d39afa
dshush <dani.shosh@gmail.com>
2013-01-13 18:07:23 +0200

    add test class for ArrayOfElements

---
+++ testsrc/arithmetic/objects/arrays/ArrayOfElementsTests.java
@@ -0,0 +1,36 @@
+package arithmetic.objects.arrays;
+
+import org.junit.Test;
+
+public class ArrayOfElementsTests {
+
+	@Test
+	public void getAt() {
+		// TODO
+	}
+
+	@Test
+	public void setAt() {
+		// TODO
+	}
+
+	@Test
+	public void add() {
+		// TODO
+	}
+
+	@Test
+	public void getSize() {
+		// TODO
+	}
+
+	@Test
+	public void equals() {
+		// TODO
+	}
+
+	@Test
+	public void toByteArray() {
+		// TODO
+	}
+}
