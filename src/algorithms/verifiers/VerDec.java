c84784594f95cc27303266d5b788687973e82250
sofiazax <sofiazax@gmail.com>
2013-01-13 19:40:51 +0200

    Continue ver dec

--- src/algorithms/verifiers/VerDec.java
+++ src/algorithms/verifiers/VerDec.java
@@ -1,5 +1,10 @@
 package algorithms.verifiers;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import arithmetic.objects.ElementsExtractor;
+import arithmetic.objects.arrays.ArrayGenerators;
 import arithmetic.objects.arrays.ArrayOfElements;
 import arithmetic.objects.basicelements.Node;
 import arithmetic.objects.groups.IGroup;
@@ -19,10 +24,11 @@ import cryptographic.primitives.RandomOracle;
 
 public class VerDec {
 
-	private ArrayOfElements<ArrayOfElements<IGroupElement>> mixDecryptionFactors;
-	private ArrayOfElements<Node> mixDecrFactCommitment;
-	private ArrayOfElements<Node> mixDecrFactReply;
-	
+	private static ArrayOfElements<ArrayOfElements<IGroupElement>> DecryptionFactors;
+	private static ArrayOfElements<Node> DecrFactCommitment;
+	private static ArrayOfElements<Node> DecrFactReply;
+
+	// TODO change the comment
 	/**
 	 * @param arrayOfElements2
 	 * @param publicKeys
@@ -43,19 +49,59 @@ public class VerDec {
 	 *         otherwise.
 	 */
 	static public boolean verify(RandomOracle ROSeed, RandomOracle ROChallenge,
-			String directory, byte[] prefixToRO, int lambda, int N, int ne, int nr, int nv,
-			PseudoRandomGenerator prg, IGroup Gq, ProductGroupElement pk,
-			ArrayOfElements<ProductGroupElement> L,
+			String directory, byte[] prefixToRO, int lambda, int N, int ne,
+			int nr, int nv, PseudoRandomGenerator prg, IGroup Gq,
+			ProductGroupElement pk, ArrayOfElements<ProductGroupElement> L,
 			ArrayOfElements<ProductRingElement> m,
 			IRing<IntegerRingElement> Zq,
 			ArrayOfElements<IGroupElement> publicKeys,
 			ArrayOfElements<IntegerRingElement> secretKeys) {
-		
-		
-		//Create the arrays of the different factors
 
-		
-		
+		// Create the arrays of the different factors
+		if (!readDecriptionFactors(lambda, directory, Gq)
+				|| !readDecrFactCommitment(lambda, directory, Gq)
+				|| !readDecrFactReply(lambda, directory, Gq))
+			return false;
+
+		return true;
+	}
+
+	private static boolean readDecrFactReply(int lambda, String directory,
+			IGroup Gq) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static boolean readDecrFactCommitment(int lambda, String directory,
+			IGroup Gq) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private static boolean readDecriptionFactors(int lambda, String directory,
+			IGroup Gq) {
+
+		for (int i = 0; i < lambda; i++) {
+
+			byte[] bDecrFact;
+			try {
+				bDecrFact = ElementsExtractor.btFromFile(directory, "proofs",
+						"DecriptionFactors" + (i < 10 ? "0" : "") + i + ".bt");
+			} catch (IOException e) {
+				return false;
+			}
+			if (bDecrFact == null)
+				return false;
+
+			try {
+				ArrayOfElements<IGroupElement> factor = ArrayGenerators
+						.createGroupElementArray(bDecrFact, Gq);
+				DecryptionFactors.add(factor);
+			} catch (UnsupportedEncodingException e) {
+				return false;
+			}
+
+		}
 		return true;
 	}
 
