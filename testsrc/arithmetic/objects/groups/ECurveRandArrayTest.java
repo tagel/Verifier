46c61743c6db4dd3cb1caa06268ad503f3b239e8
dshush <dani.shosh@gmail.com>
2013-01-15 20:12:20 +0200

    fixed a test in ECurveRandArrayTest after adding the Type Enum

--- testsrc/arithmetic/objects/groups/ECurveRandArrayTest.java
+++ testsrc/arithmetic/objects/groups/ECurveRandArrayTest.java
@@ -60,7 +60,7 @@ public class ECurveRandArrayTest {
 		Parameters params = new Parameters(getClass().getClassLoader()
 				.getResource("protInfo.xml").getFile(), getClass()
 				.getClassLoader().getResource("export/default").getFile(),
-				"type", "auxsid", 1, false, false, false);
+				null, "auxsid", 1, false, false, false);
 		Assert.assertNotNull("res is not in the classpath - ask Daniel",
 				getClass().getClassLoader()
 						.getResource("export/default/proofs"));
