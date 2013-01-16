ad105d5d804b5c75661985fc520dcd7dbc6a55d3
Itay <itayva@hotmail.com>
2013-01-16 20:09:02 +0200

    .

--- src/arithmetic/objects/ElementsExtractor.java
+++ src/arithmetic/objects/ElementsExtractor.java
@@ -1,16 +1,13 @@
 package arithmetic.objects;
 
-
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import arithmetic.objects.LargeInteger;
 import java.util.Arrays;
-import cryptographic.primitives.CryptoUtils;
+
 import arithmetic.objects.arrays.ArrayGenerators;
 import arithmetic.objects.arrays.ArrayOfElements;
 import arithmetic.objects.basicelements.Node;
@@ -25,14 +22,11 @@ import arithmetic.objects.groups.ModGroup;
 import arithmetic.objects.groups.ModGroupElement;
 import arithmetic.objects.groups.Point;
 import arithmetic.objects.groups.ProductGroupElement;
-
-
-
-
+import cryptographic.primitives.CryptoUtils;
 
 /**
  * This class has methods which help us extract elements from given byte arrays.
- *
+ * 
  */
 public class ElementsExtractor {
 
@@ -40,39 +34,44 @@ public class ElementsExtractor {
 	 * @param byte array arr that represents a leaf of an integer
 	 * @return the integer that the byte array represents.
 	 */
-	public static LargeInteger leafToInt (byte[] arr) { 
+	public static LargeInteger leafToInt(byte[] arr) {
 		byte[] a = Arrays.copyOfRange(arr, 5, arr.length);
 		return new LargeInteger(a);
 	}
 
-
 	/**
 	 * @param byte array arr that represents a leaf of a string.
 	 * @return the string that arr represents.
-	 * @throws UnsupportedEncodingException 
+	 * @throws UnsupportedEncodingException
 	 */
-	public static String leafToString (byte[] arr) throws UnsupportedEncodingException {
+	public static String leafToString(byte[] arr)
+			throws UnsupportedEncodingException {
 		byte[] a = Arrays.copyOfRange(arr, 5, arr.length);
 		return new String(a, "ASCII");
 	}
 
 	/**
 	 * 
-	 * @param arr = a byte array that represents a point.
+	 * @param arr
+	 *            = a byte array that represents a point.
 	 * @return the point that arr represents.
 	 */
-	public static Point nodeToPoint (byte[] arr, IGroup group) {
-		byte[] arrX = Arrays.copyOfRange(arr, 5, 10+group.getFieldOrder().toByteArray().length);
-		byte[] arrY = Arrays.copyOfRange(arr, 10+group.getFieldOrder().toByteArray().length, arr.length);
-		IField<IntegerFieldElement> field = new PrimeOrderField(group.getFieldOrder());
+	public static Point nodeToPoint(byte[] arr, IGroup group) {
+		byte[] arrX = Arrays.copyOfRange(arr, 5, 10 + group.getFieldOrder()
+				.toByteArray().length);
+		byte[] arrY = Arrays.copyOfRange(arr, 10 + group.getFieldOrder()
+				.toByteArray().length, arr.length);
+		IField<IntegerFieldElement> field = new PrimeOrderField(
+				group.getFieldOrder());
 		IntegerFieldElement x = new IntegerFieldElement(leafToInt(arrX), field);
 		IntegerFieldElement y = new IntegerFieldElement(leafToInt(arrY), field);
-		return new Point(x,y);
-		
+		return new Point(x, y);
+
 	}
 
 	// if file is not found, returns null.
-	public static byte[] btFromFile (String path, String filename) throws IOException {
+	public static byte[] btFromFile(String path, String filename)
+			throws IOException {
 		try {
 			File file = new File(path, filename);
 			InputStream stream = new FileInputStream(file);
@@ -80,12 +79,14 @@ public class ElementsExtractor {
 			stream.read(b, 0, b.length);
 			stream.close();
 			return b;
+		} catch (FileNotFoundException e) {
+			return null;
 		}
-		catch (FileNotFoundException e) { return null;}
 
 	}
-	
-	public static byte[] btFromFile (String path, String subpath, String filename) throws IOException {
+
+	public static byte[] btFromFile(String path, String subpath, String filename)
+			throws IOException {
 		try {
 			File f1 = new File(path, subpath);
 			File file = new File(f1, filename);
@@ -94,16 +95,15 @@ public class ElementsExtractor {
 			stream.read(b, 0, b.length);
 			stream.close();
 			return b;
+		} catch (FileNotFoundException e) {
+			return null;
 		}
-		catch (FileNotFoundException e) { return null;}
 
 	}
-	
-	
 
-	public static IGroupElement createGroupElement (byte[] b, IGroup Gq ) {
+	public static IGroupElement createGroupElement(byte[] b, IGroup Gq) {
 		if (Gq instanceof ModGroup)
-			return new ModGroupElement(leafToInt(b),(ModGroup) Gq);
+			return new ModGroupElement(leafToInt(b), (ModGroup) Gq);
 		if (Gq instanceof ECurveGroup)
 			return new ECurveGroupElement(nodeToPoint(b, Gq), (ECurveGroup) Gq);
 		else {
@@ -111,33 +111,45 @@ public class ElementsExtractor {
 			return null;
 		}
 	}
-	
-	public static ProductGroupElement createSimplePGE (byte[] bt, IGroup group) throws UnsupportedEncodingException {
-		ArrayOfElements<IGroupElement> arr = ArrayGenerators.createGroupElementArray (bt, group);
+
+	public static ProductGroupElement createSimplePGE(byte[] bt, IGroup group)
+			throws UnsupportedEncodingException {
+		ArrayOfElements<IGroupElement> arr = ArrayGenerators
+				.createGroupElementArray(bt, group);
 		return new ProductGroupElement(arr);
 	}
-	
-	public static ProductGroupElement createCiphertext (byte[] bt, IGroup group) throws UnsupportedEncodingException {
+
+	public static ProductGroupElement createCiphertext(byte[] bt, IGroup group)
+			throws UnsupportedEncodingException {
 		Node node = new Node(bt);
-		ProductGroupElement left = createSimplePGE(node.getAt(0).toByteArray(), group);
-		ProductGroupElement right = createSimplePGE(node.getAt(1).toByteArray(), group);
+		ProductGroupElement left = createSimplePGE(node.getAt(0).toByteArray(),
+				group);
+		ProductGroupElement right = createSimplePGE(
+				node.getAt(1).toByteArray(), group);
 		return new ProductGroupElement(left, right);
 	}
 
-	public static ProductGroupElement createCiphertext (ArrayOfElements<IGroupElement> left, ArrayOfElements<IGroupElement> right) {
-		return new ProductGroupElement(new ProductGroupElement(left), new ProductGroupElement(right));
+	public static ProductGroupElement createCiphertext(
+			ArrayOfElements<IGroupElement> left,
+			ArrayOfElements<IGroupElement> right) {
+		return new ProductGroupElement(new ProductGroupElement(left),
+				new ProductGroupElement(right));
 	}
-
 
 	/**
 	 * @param <E>
-	 * @param a string s, representing a certain group.
-	 * @return the group recovered from s by removing the comment and colons, converting the hexa string to a byte array, converting the byte array into a byte tree, and converting the byte tree into the group.
-	 * @throws UnsupportedEncodingException 
+	 * @param a
+	 *            string s, representing a certain group.
+	 * @return the group recovered from s by removing the comment and colons,
+	 *         converting the hexa string to a byte array, converting the byte
+	 *         array into a byte tree, and converting the byte tree into the
+	 *         group.
+	 * @throws UnsupportedEncodingException
 	 */
-	public static IGroup unmarshal (String s) throws UnsupportedEncodingException  {
+	public static IGroup unmarshal(String s)
+			throws UnsupportedEncodingException {
 		int i = s.indexOf(":");
-		s = s.substring(i+2, s.length());
+		s = s.substring(i + 2, s.length());
 		byte[] b = CryptoUtils.hexStringToBytes(s);
 		Node node = new Node(b);
 		String type = leafToString(node.getAt(0).toByteArray());
@@ -145,11 +157,11 @@ public class ElementsExtractor {
 			return new ModGroup(node.getAt(1).toByteArray());
 		if (type.equals("verificatum.arithm.ECqPGroup"))
 			return new ECurveGroup(leafToString(node.getAt(1).toByteArray()));
-		else { 
-			System.out.println("ERROR: name of java class is unrecognized by the system");
+		else {
+			System.out
+					.println("ERROR: name of java class is unrecognized by the system");
 			return null;
 		}
 	}
-	
-	
+
 }
--- src/arithmetic/objects/arrays/ArrayGenerators.java
+++ src/arithmetic/objects/arrays/ArrayGenerators.java
@@ -14,61 +14,78 @@ import arithmetic.objects.ring.ProductRingElement;
 
 public class ArrayGenerators {
 
-
-
-	public static ArrayOfElements<IGroupElement> createGroupElementArray (byte[] b, IGroup group) throws UnsupportedEncodingException {
+	public static ArrayOfElements<IGroupElement> createGroupElementArray(
+			byte[] b, IGroup group) throws UnsupportedEncodingException {
 		ArrayOfElements<IGroupElement> ret = new ArrayOfElements<IGroupElement>();
 		Node node = new Node(b);
-		for (int i=0; i<node.getChildrenSize(); i++)
-			ret.add(ElementsExtractor.createGroupElement(node.getAt(i).toByteArray(), group));
+		for (int i = 0; i < node.getChildrenSize(); i++)
+			ret.add(ElementsExtractor.createGroupElement(node.getAt(i)
+					.toByteArray(), group));
 		return ret;
 	}
 
-	public static ArrayOfElements<IntegerRingElement> createRingElementArray (byte[] b, IRing<IntegerRingElement> ring) throws UnsupportedEncodingException {
+	public static ArrayOfElements<IntegerRingElement> createRingElementArray(
+			byte[] b, IRing<IntegerRingElement> ring)
+			throws UnsupportedEncodingException {
 		ArrayOfElements<IntegerRingElement> ret = new ArrayOfElements<IntegerRingElement>();
 		Node node = new Node(b);
-		for (int i=0; i<node.getChildrenSize(); i++) 
-			ret.add(new IntegerRingElement(ElementsExtractor.leafToInt(node.getAt(i).toByteArray()), ring));
+		for (int i = 0; i < node.getChildrenSize(); i++)
+			ret.add(new IntegerRingElement(ElementsExtractor.leafToInt(node
+					.getAt(i).toByteArray()), ring));
 		return ret;
 	}
 
-	public static ArrayOfElements<ProductGroupElement> createArrayOfCiphertexts (byte[] data, IGroup group, int w) throws UnsupportedEncodingException {
+	public static ArrayOfElements<ProductGroupElement> createArrayOfCiphertexts(
+			byte[] data, IGroup group, int w)
+			throws UnsupportedEncodingException {
 		ArrayOfElements<ProductGroupElement> ret = new ArrayOfElements<ProductGroupElement>();
 		Node node = new Node(data);
-		int arraySize = (new Node(node.getAt(0).toByteArray())).getChildrenSize();
-		if (w==1 && new Node(new Node(node.getAt(0).toByteArray()).getAt(0).toByteArray()).getChildrenSize()==2) {
-			ArrayOfElements<IGroupElement> leftArr = createGroupElementArray(node.getAt(0).toByteArray(), group);
-			ArrayOfElements<IGroupElement> rightArr = createGroupElementArray(node.getAt(1).toByteArray(), group);
-			for (int i=0; i<arraySize; i++) {
+		int arraySize = (new Node(node.getAt(0).toByteArray()))
+				.getChildrenSize();
+		if (w == 1
+				&& new Node(new Node(node.getAt(0).toByteArray()).getAt(0)
+						.toByteArray()).getChildrenSize() == 2) {
+			ArrayOfElements<IGroupElement> leftArr = createGroupElementArray(
+					node.getAt(0).toByteArray(), group);
+			ArrayOfElements<IGroupElement> rightArr = createGroupElementArray(
+					node.getAt(1).toByteArray(), group);
+			for (int i = 0; i < arraySize; i++) {
 				ArrayOfElements<IGroupElement> arr1 = new ArrayOfElements<IGroupElement>();
 				arr1.add(leftArr.getAt(i));
 				ArrayOfElements<IGroupElement> arr2 = new ArrayOfElements<IGroupElement>();
 				arr2.add(rightArr.getAt(i));
-				ProductGroupElement ciphertext = ElementsExtractor.createCiphertext(arr1, arr2);
+				ProductGroupElement ciphertext = ElementsExtractor
+						.createCiphertext(arr1, arr2);
 				ret.add(ciphertext);
 			}
-		}
-		else {
-			for (int i=0; i<arraySize; i++) {
-				ProductGroupElement simplePGEleft = ElementsExtractor.createSimplePGE(new Node(node.getAt(0).toByteArray()).getAt(i).toByteArray(), group);
-				ProductGroupElement simplePGEright = ElementsExtractor.createSimplePGE(new Node(node.getAt(1).toByteArray()).getAt(i).toByteArray(), group);
-				ProductGroupElement ciphertext = new ProductGroupElement(simplePGEleft, simplePGEright);
+		} else {
+			for (int i = 0; i < arraySize; i++) {
+				ProductGroupElement simplePGEleft = ElementsExtractor
+						.createSimplePGE(new Node(node.getAt(0).toByteArray())
+								.getAt(i).toByteArray(), group);
+				ProductGroupElement simplePGEright = ElementsExtractor
+						.createSimplePGE(new Node(node.getAt(1).toByteArray())
+								.getAt(i).toByteArray(), group);
+				ProductGroupElement ciphertext = new ProductGroupElement(
+						simplePGEleft, simplePGEright);
 				ret.add(ciphertext);
 			}
 		}
 		return ret;
 	}
 
-
-	public static ArrayOfElements<ProductRingElement> createArrayOfPlaintexts (byte[] data, IRing<IntegerRingElement> ring) throws UnsupportedEncodingException {
+	public static ArrayOfElements<ProductRingElement> createArrayOfPlaintexts(
+			byte[] data, IRing<IntegerRingElement> ring)
+			throws UnsupportedEncodingException {
 		ArrayOfElements<ProductRingElement> ret = new ArrayOfElements<ProductRingElement>();
 		Node node = new Node(data);
 		int w = node.getChildrenSize();
 		int size = (new Node(node.getAt(0).toByteArray())).getChildrenSize();
-		for (int i=0; i<size; i++) {
+		for (int i = 0; i < size; i++) {
 			ArrayOfElements<IntegerRingElement> arr = new ArrayOfElements<IntegerRingElement>();
-			for (int j=0; j<w; j++) {
-				LargeInteger a = ElementsExtractor.leafToInt(new Node(node.getAt(j).toByteArray()).getAt(i).toByteArray());
+			for (int j = 0; j < w; j++) {
+				LargeInteger a = ElementsExtractor.leafToInt(new Node(node
+						.getAt(j).toByteArray()).getAt(i).toByteArray());
 				IntegerRingElement element = new IntegerRingElement(a, ring);
 				arr.add(element);
 			}
@@ -77,9 +94,8 @@ public class ArrayGenerators {
 		return ret;
 	}
 
-
 	public static byte[] concatArrays(byte[] A, byte[] B) {
-		byte[] C= new byte[A.length+B.length];
+		byte[] C = new byte[A.length + B.length];
 		System.arraycopy(A, 0, C, 0, A.length);
 		System.arraycopy(B, 0, C, A.length, B.length);
 		return C;
