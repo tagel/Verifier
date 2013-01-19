60225c754561eb521b99971a29c0f09821b2cec0
dshush <dani.shosh@gmail.com>
2013-01-19 11:38:36 +0200

    add tests to CommandLineParser and add fixed some issues add doc to CryptoUtils

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,5 @@
 package main;
 
-import java.io.IOException;
-
 import algorithms.verifiers.MainVerifier;
 
 /**
@@ -17,26 +15,24 @@ public class Main {
 	public static void main(String[] argv) {
 		CommandLineParser parser = new CommandLineParser();
 		parser.parseCommand(argv);
+		
 		if (parser.shouldVerify()) {
 			try {
 				callMainVerifier(parser);
-			} catch (IOException e) {
-				// TODO Daniel - talk to sofi (shouldn't throw exception)
+			} catch (Exception e) {
+				// TODO Daniel - talk to Sofi, shouldn't trow
 				e.printStackTrace();
 			}
+
 		}
 	}
 
 	private static void callMainVerifier(CommandLineParser parser)
-			throws IOException {
+			throws Exception {
 		MainVerifier verifier = new MainVerifier();
-		try {
-			verifier.verify(parser.getXml(), parser.getDir(), parser.getType(),
-					parser.getAuxsid(), parser.getWidth(), parser.getPosc(),
-					parser.getCcpos(), parser.getDec());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		verifier.verify(parser.getXml(), parser.getDir(), parser.getType(),
+				parser.getAuxsid(), parser.getWidth(), parser.getPosc(),
+				parser.getCcpos(), parser.getDec());
+
 	}
 }
