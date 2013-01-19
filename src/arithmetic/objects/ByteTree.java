d7762e867c69d33c749c4cbccbc7d324e75b2f0e
Itay <itayva@hotmail.com>
2013-01-19 18:16:23 +0200

    .

--- src/arithmetic/objects/ByteTree.java
+++ src/arithmetic/objects/ByteTree.java
@@ -1,13 +1,18 @@
 package arithmetic.objects;
 
 /**
- * This is a "wrapper" interface used to denote each arithmetic object as a byte tree. Thus, every arithmetic object is going to implement this interface.
+ * This is a "wrapper" interface used to denote each arithmetic object as a byte
+ * tree. Thus, every arithmetic object is going to implement this interface.
+ * (field, ring, and group elements, product elements, arrays of elements,
+ * nodes, leaves, etc...
  */
 
 public interface ByteTree {
-	
-	
+
+	/**
+	 * 
+	 * @return the byte array representation (as a byte tree) of the object.
+	 */
 	public byte[] toByteArray();
-		
 
 }
