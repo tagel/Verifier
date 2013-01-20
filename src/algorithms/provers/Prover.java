d2b910757dc9c13cdd7920a18414d32fde66b40d
tagel <tagelshaked@gmail.com>
2013-01-20 19:46:06 +0200

    finished comments + refactoring to the provers

--- src/algorithms/provers/Prover.java
+++ src/algorithms/provers/Prover.java
@@ -13,24 +13,44 @@ import arithmetic.objects.ring.ProductRingElement;
 import cryptographic.primitives.PseudoRandomGenerator;
 import cryptographic.primitives.RandomOracle;
 
+/**
+ * This class is an abstract class which concludes the mutual code for all the
+ * provers
+ * 
+ * @author tagel
+ */
 public abstract class Prover {
 
 	protected static final String GENERATORS = "generators";
 
 	/**
-	 * This function decrypts a given ciphertext back to its plaintext. Input
-	 * description is as follows: x - Additive inverse of the exponent by which
-	 * the standard generator in the public key is raised u - First part of
-	 * ciphertext to be decrypted
+	 * This function decrypts a given ciphertext back to its decryption factor
+	 * 
+	 * @param x
+	 *            Additive inverse of the exponent by which the standard
+	 *            generator in the public key is raised
+	 * @param A
+	 *            ciphertext to be decrypted
+	 * @return the decryption factor
 	 */
 	public static ProductGroupElement PDecrypt(IntegerRingElement x,
-			ProductGroupElement A)  {
+			ProductGroupElement A) {
 		return A.getLeft().power(x.neg().getElement());
 	}
-	
+
+	/**
+	 * This function decrypts a given array of ciphertexts back to their
+	 * decryption factors
+	 * 
+	 * @param x
+	 *            array of the additive inverse of the exponent by which the
+	 *            standard generator in the public key is raised
+	 * @param A
+	 *            array of the ciphertexts to be decrypted
+	 * @return array of decryption factors
+	 */
 	public static ArrayOfElements<ProductGroupElement> PDecrypt(
-			IntegerRingElement x, ArrayOfElements<ProductGroupElement> A)
-			 {
+			IntegerRingElement x, ArrayOfElements<ProductGroupElement> A) {
 
 		ArrayOfElements<ProductGroupElement> res = new ArrayOfElements<ProductGroupElement>();
 		for (int i = 0; i < A.getSize(); i++) {
@@ -39,11 +59,29 @@ public abstract class Prover {
 		return res;
 	}
 
+	/**
+	 * This function does a trivial decryption
+	 * 
+	 * @param A
+	 *            ciphertext to be decrypted
+	 * @param f
+	 *            decrypion factor
+	 * @return m the plaintext
+	 */
 	public static ProductGroupElement TDecrypt(ProductGroupElement A,
 			ProductGroupElement f) {
 		return A.getRight().mult(f);
 	}
 
+	/**
+	 * This function does a trivial decryption
+	 * 
+	 * @param A
+	 *            array of ciphertexts to be decrypted
+	 * @param f
+	 *            array of decrypion factors
+	 * @return array of plaintexts
+	 */
 	public static ArrayOfElements<ProductGroupElement> TDecrypt(
 			ArrayOfElements<ProductGroupElement> L,
 			ArrayOfElements<ProductGroupElement> f) {
@@ -59,12 +97,15 @@ public abstract class Prover {
 	 * This function encrypts the message m using the public key pk, and returns
 	 * the CipherText.
 	 * 
-	 * @param pk
-	 *            - Public key (g, y) used to encrypt the message
-	 * @param s
-	 * @param Gq
 	 * @param m
-	 * @return
+	 *            plaintexts
+	 * @param pk
+	 *            Public key (g, y) used to encrypt the message
+	 * @param s
+	 *            random element in Zq
+	 * @param Gq
+	 *            the group
+	 * @return ciphertext, encryption of the plaintext
 	 */
 	public static ProductGroupElement encrypt(ProductGroupElement m,
 			ProductRingElement s, ProductGroupElement pk, IGroup Gq) {
@@ -80,8 +121,8 @@ public abstract class Prover {
 			right.add((y.power(powers.getAt(i).getElement()).mult(ms.getAt(i))));
 		}
 
-		ProductGroupElement encryptedMsg = new ProductGroupElement(new ProductGroupElement(left),
-			new ProductGroupElement(right));
+		ProductGroupElement encryptedMsg = new ProductGroupElement(
+				new ProductGroupElement(left), new ProductGroupElement(right));
 		return encryptedMsg;
 	}
 
@@ -101,7 +142,16 @@ public abstract class Prover {
 		return ROSeed.getRandomOracleOutput(ArrayGenerators.concatArrays(ro,
 				nodeForSeed.toByteArray()));
 	}
-	
+
+	/**
+	 * This function makes sure 0 < v < 2^Nv
+	 * 
+	 * @param Nv
+	 *            number of bits in challenge
+	 * @param challenge
+	 *            the challenge itself
+	 * @return a number 0 < v < 2^Nv
+	 */
 	protected static LargeInteger computeV(int Nv, byte[] challenge) {
 		/* Computation of v: */
 		LargeInteger v = new LargeInteger(challenge);
@@ -109,7 +159,20 @@ public abstract class Prover {
 		v = v.mod(twoNv);
 		return v;
 	}
-	
+
+	/**
+	 * This function computes the challenge used in the proof
+	 * 
+	 * @param ROChallenge
+	 *            RandomOracle for computing the challenge
+	 * @param ro
+	 *            prefix to random oracle
+	 * @param node
+	 *            Node used for the RO's input
+	 * @param leaf
+	 *            leaf used for the RO's input
+	 * @return the challenge v as a byte array
+	 */
 	protected static byte[] computeChallenge(RandomOracle ROChallenge,
 			byte[] ro, Node node, ByteTree leaf) {
 		ByteTree[] inputChallenge = new ByteTree[2];
@@ -117,11 +180,11 @@ public abstract class Prover {
 		inputChallenge[1] = node;
 		Node nodeForChallenge = new Node(inputChallenge);
 
-		byte[] challenge = ROChallenge
-				.getRandomOracleOutput(ArrayGenerators.concatArrays(ro,
-						nodeForChallenge.toByteArray()));
+		byte[] challenge = ROChallenge.getRandomOracleOutput(ArrayGenerators
+				.concatArrays(ro, nodeForChallenge.toByteArray()));
 		return challenge;
 	}
+
 	/**
 	 * This function computes A, needed in the proof.
 	 * 
@@ -206,7 +269,7 @@ public abstract class Prover {
 	 *            batching
 	 * @param wInput
 	 *            Array of input ciphertexts
-	 * @return F, the multiplication of Wi^Ei N times
+	 * @return F, the multiplication of WInput^ei N times
 	 */
 	protected static ProductGroupElement computeF(int N, int Ne, byte[] seed,
 			PseudoRandomGenerator prg,
@@ -237,8 +300,8 @@ public abstract class Prover {
 	 *            Array of random elements used to compute the seed
 	 * @param N
 	 *            Size of the arrays
-	 * @return C, the multiplication of Ui N times divided by multiplication of
-	 *         hi N times
+	 * @return C, the multiplication of Ui elements divided by multiplication of
+	 *         hi elements
 	 */
 	protected static IGroupElement computeC(ArrayOfElements<IGroupElement> u,
 			ArrayOfElements<IGroupElement> h, int N) {
@@ -264,7 +327,7 @@ public abstract class Prover {
 	 *            Array of random elements used to compute the seed
 	 * @param N
 	 *            size of the arrays
-	 * @return D, the multiplication of Ui N times
+	 * @return D, B[n-1] divided by h0^(the multiplication of e's elements)
 	 */
 	protected static IGroupElement computeD(LargeInteger E,
 			ArrayOfElements<IGroupElement> B, ArrayOfElements<IGroupElement> h,
@@ -275,7 +338,7 @@ public abstract class Prover {
 	}
 
 	/**
-	 * This function verifies the first equation: A^v * Atag = (g^ka) *
+	 * This function verifies the first equation: A^v * Atag == (g^ka) *
 	 * PI(h[i]^ke[i])
 	 * 
 	 * @param A
@@ -313,6 +376,27 @@ public abstract class Prover {
 		return true;
 	}
 
+	/**
+	 * This function verifies the equation: B^v * Btag == g^kbi * B[i-1]^Kei
+	 * 
+	 * @param B
+	 *            Array of N elements in Gq
+	 * @param Btag
+	 *            Array of N elements in Gq
+	 * @param Kb
+	 *            array of N elements in Zq
+	 * @param Ke
+	 *            array of N elements in Zq
+	 * @param g
+	 *            generator of Gq
+	 * @param v
+	 *            challenge computed by the Random Oracle
+	 * @param h
+	 *            Array of random elements used to compute the seed
+	 * @param N
+	 *            size of the arrays
+	 * @return true if the equation is correct and false otherwise.
+	 */
 	protected static boolean verifyBvBtag(ArrayOfElements<IGroupElement> B,
 			ArrayOfElements<IGroupElement> Btag,
 			ArrayOfElements<IntegerRingElement> Kb,
@@ -336,6 +420,21 @@ public abstract class Prover {
 		return true;
 	}
 
+	/**
+	 * This function verifies the equation: C^v * Ctag == g^kc
+	 * 
+	 * @param C
+	 *            the multiplication of u elements
+	 * @param Ctag
+	 *            element in Gq
+	 * @param v
+	 *            challenge computed by the Random Oracle
+	 * @param Kc
+	 *            element in Zq
+	 * @param g
+	 *            generator of Gq
+	 * @return true if the equation is correct and false otherwise.
+	 */
 	protected static boolean verifyCvCtag(IGroupElement C, IGroupElement Ctag,
 			LargeInteger v, IntegerRingElement Kc, IGroupElement g) {
 		IGroupElement left = (C.power(v)).mult(Ctag);
@@ -346,11 +445,80 @@ public abstract class Prover {
 		return true;
 	}
 
+	/**
+	 * This function verifies the equation: D^v * Dtag == g^kd
+	 * 
+	 * @param D
+	 *            the multiplication of h elements
+	 * @param Dtag
+	 *            element in Gq
+	 * @param v
+	 *            challenge computed by the Random Oracle
+	 * @param Kd
+	 *            element in Zq
+	 * @param g
+	 *            generator of Gq
+	 * @return true if the equation is correct and false otherwise.
+	 */
 	protected static boolean verifyDvDtag(IGroupElement D, IGroupElement Dtag,
 			LargeInteger v, IntegerRingElement Kd, IGroupElement g) {
 		IGroupElement left = (D.power(v)).mult(Dtag);
 		IGroupElement right = g.power(Kd.getElement());
 		if (!left.equals(right)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * This function verifies the equation: F^v * Ftag == ENCpk(1,-Kf) *
+	 * PI((wi)^Ke)
+	 * 
+	 * @param N
+	 *            size of the arrays
+	 * @param Gq
+	 *            The group
+	 * @param pk
+	 *            The public key
+	 * @param wOutput
+	 *            Array of output ciphertexts
+	 * @param width
+	 *            width of plaintexts and ciphertexts
+	 * @param Ftag
+	 *            ciphertext
+	 * @param Kf
+	 *            ciphertext
+	 * @param Ke
+	 *            array of N elements in Zq
+	 * @param F
+	 *            the multiplication of WInput^ei N times
+	 * @param v
+	 *            challenge computed by the Random Oracle
+	 * @return true if the equation is correct and false otherwise.
+	 */
+	protected static boolean verifyFFtag(int N, IGroup Gq,
+			ProductGroupElement pk,
+			ArrayOfElements<ProductGroupElement> wOutput, int width,
+			ProductGroupElement Ftag, ProductRingElement Kf,
+			ArrayOfElements<IntegerRingElement> Ke, ProductGroupElement F,
+			LargeInteger v) {
+		ProductGroupElement leftF = F.power(v).mult(Ftag);
+
+		ProductGroupElement W = wOutput.getAt(0)
+				.power(Ke.getAt(0).getElement());
+		for (int i = 1; i < N; i++) {
+			W = W.mult(wOutput.getAt(i).power(Ke.getAt(i).getElement()));
+		}
+
+		// create ProductGroupElement of 1s
+		ArrayOfElements<IGroupElement> arrOfOnes = new ArrayOfElements<IGroupElement>();
+		for (int i = 0; i < width; i++) {
+			arrOfOnes.add(Gq.one());
+		}
+
+		ProductGroupElement ones = new ProductGroupElement(arrOfOnes);
+		ProductGroupElement rigthF = encrypt(ones, Kf, pk, Gq);
+		if (!leftF.equals(rigthF)) {
 			return false;
 		}
 		return true;
